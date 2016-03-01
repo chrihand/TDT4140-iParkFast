65772d1ed94ed59789b5047d2b810fdae226a5f8
Christina Hellenes Andresen <chrihand@stud.ntnu.no>
2016-03-01 12:20:19 +0100

    Added @app.route('/timer')

--- TDT4140-iParkFast.py
+++ TDT4140-iParkFast.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('main.html')
 
+@app.route('/timer')
+def timer():
+    return render_template('timer.html')
 
 if __name__ == '__main__':
     app.run()
