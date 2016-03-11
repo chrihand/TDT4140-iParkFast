b1aef0a32c4f27fb473e18c31e36d57cbacc65bf
Tibor Vukovic <tiborvukovic91@gmail.com>
2016-03-11 13:37:52 +0100

    Add session example

--- TDT4140-iParkFast.py
+++ TDT4140-iParkFast.py
@@ -1,17 +1,25 @@
-from flask import Flask
+import flask
 from flask import render_template
+import datetime
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 app._static_folder = "static"
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 
 @app.route('/')
 def hello_world():
+    flask.session['future'] = datetime.datetime.now() + datetime.timedelta(minutes=5)
     return render_template('main.html')
 
-@app.route('/timer')
+
+@app.route('/check')
 def timer():
-    return render_template('timer.html')
+    now = datetime.datetime.now()
+    future = flask.session['future']
+    return render_template('timer.html', hasended=future>now, ends=future)
+
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000, debug=True)
